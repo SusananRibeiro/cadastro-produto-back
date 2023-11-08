59c8690b7196666a01b42bc93aa7ef0be661fc53
Susana_Ribeiro <susanadonribeiro@gmail.com>
2023-11-08 11:09:46 -0300

    Atualizando a branch master com a criacao do CI/CD

---
+++ src/test/java/com/cadastro/api/CadastroProdutoBackApplicationTest.java
@@ -0,0 +1,11 @@
+package com.cadastro.api;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class CadastroProdutoBackApplicationTest {
+
+    @Test
+    void contextLoads() {
+    }
+}
